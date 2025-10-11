938cdfb6624603d8cd02c01a74a3381b5e3cc8c8
1jiwoo27 <chloey1227@naver.com>
2025-10-12 01:17:57 +0900

    feat: 인터셉터 설정

---
+++ src/shared/api/axios.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.warn("토큰이 만료되었습니다.");
+    }
+    return Promise.reject(error);
+  }
+);
