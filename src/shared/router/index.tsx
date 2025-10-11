c9500a9f79ca1d965e83da5f3dc9dfc1e69c526c
1jiwoo27 <chloey1227@naver.com>
2025-10-12 00:53:02 +0900

    feat: 경로 설정

---
+++ src/shared/router/index.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import { PATH } from "./path";
+import HomePage from "../../pages/home/home-page";
+import LoginPage from "../../pages/login/login-page";
+import ErrorPage from "../../pages/error/error-page";
+
+export const router = createBrowserRouter([
+  { path: PATH.HOME, element: <HomePage /> },
+  { path: PATH.LOGIN, element: <LoginPage /> },
+  { path: PATH.NOT_FOUND, element: <ErrorPage /> },
+]);
