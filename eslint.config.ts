0823652efe13fc36247d5b1df456941178528d75
jihun3666 <jihun3666@gmail.com>
2025-10-12 20:42:09 +0900

    init: change request 반영

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,8 +6,15 @@ import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser }
+  },
+  js.configs.recommended,
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  {
+    files: ["**/*.json"],
+    ...json.configs.recommended,
+  },
 ]);
