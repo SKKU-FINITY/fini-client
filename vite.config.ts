f9d1dcb8d88f3a8591ab043253ca854c8fb99818
jihun3666 <jihun3666@gmail.com>
2025-10-30 22:10:04 +0900

    init: 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from 'path'
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
