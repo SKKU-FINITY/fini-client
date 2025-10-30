3d5ee883856ae639cffebdfe946ce177cae4d765
jihun3666 <jihun3666@gmail.com>
2025-10-30 22:30:43 +0900

    init: esModuleInterop 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from 'path'
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
-import { fileURLToPath } from "url";
-
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
