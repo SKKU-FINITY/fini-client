80f2274e3bd64cbf580d2d4348bfe7ebf1ff1cdd
jihun3666 <jihun3666@gmail.com>
2025-11-06 13:34:14 +0900

    init: svg 세팅

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "../shared/router";
+//import reactLogo from "../assets/svg/react.svg";
 
-const App = () => <RouterProvider router={router} />;
+const App = () => {
+    return <RouterProvider router={router} />;
+}
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [
+    react(),
+    vanillaExtractPlugin(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
 });
