75f15488478f61fe776ca0577cbbfae413b41937
1jiwoo27 <chloey1227@naver.com>
2025-11-08 00:30:40 +0900

    feat: input 컴포넌트 구현

---
+++ src/shared/components/input/input.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import type { ChangeEventHandler, InputHTMLAttributes } from 'react';
+import * as styles from './input.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEyeSlash, faEye } from '@fortawesome/free-solid-svg-icons';
+
+interface InputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
+  label?: string;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+  showIcon?: boolean;
+}
+
+const Input = ({
+  label,
+  placeholder,
+  type = 'text',
+  value,
+  showIcon,
+  onChange,
+  ...rest
+}: InputProps) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const isPassword = type === 'password';
+
+  const toggleShowPassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  return (
+    <div className={styles.container}>
+      {label && <label className={styles.label}>{label}</label>}
+
+      <div className={styles.inputWrapper}>
+        <input
+          type={isPassword ? (showPassword ? 'text' : 'password') : type}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          className={styles.input}
+          {...rest}
+        />
+        {isPassword && showIcon && (
+          <span className={styles.icon} onClick={toggleShowPassword}>
+            {showPassword ? (
+              <FontAwesomeIcon icon={faEyeSlash} />
+            ) : (
+              <FontAwesomeIcon icon={faEye} />
+            )}
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Input;
