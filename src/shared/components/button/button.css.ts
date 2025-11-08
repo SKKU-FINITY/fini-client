377f4fbe2c302b85fb946e0221aeefb1c76896b1
1jiwoo27 <101466143+1jiwoo27@users.noreply.github.com>
2025-11-08 21:01:25 +0900

    [Feat] Button 컴포넌트 구현 (#17)
    * chore: gitkeep 삭제
    
    * feat: recipe 패키지 설치
    
    * feat: button 컴포넌트 구현

---
+++ src/shared/components/button/button.css.ts
@@ -0,0 +1,50 @@
+import { recipe } from '@vanilla-extract/recipes';
+import { vars } from '../../../styles/theme.css';
+
+export const button = recipe({
+  base: {
+    width: '50rem',
+    padding: '1rem',
+    borderRadius: '0.5rem',
+    fontWeight: vars.weight.medium,
+    textAlign: 'center',
+    fontSize: vars.size.xs,
+    border: 'none',
+    cursor: 'pointer',
+    transition: 'background-color 0.2s',
+
+    selectors: {
+      '&:disabled': {
+        backgroundColor: vars.color.gray,
+        color: vars.color.white,
+        cursor: 'not-allowed',
+        opacity: 0.6,
+      },
+    },
+  },
+  variants: {
+    variant: {
+      pink: {
+        backgroundColor: vars.color.pink200,
+        color: vars.color.white,
+      },
+      blue: {
+        backgroundColor: vars.color.blue200,
+        color: vars.color.white,
+      },
+      secondary: {
+        width: 'auto',
+        padding: '0',
+        border: 'none',
+        backgroundColor: 'transparent',
+        color: vars.color.blue200,
+        fontSize: vars.size.xs,
+        fontWeight: vars.weight.regular,
+        textDecoration: 'underline',
+      },
+    },
+  },
+  defaultVariants: {
+    variant: 'pink',
+  },
+});
