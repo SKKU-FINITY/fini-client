3994becc9911d05069fbbfb0a41d8c9d217e93b2
jihun3666 <jihun3666@gmail.com>
2025-11-11 16:46:42 +0900

    feat: 홈페이지 디자인 수정

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -4,7 +4,7 @@ import Header from '../../shared/components/header/header';
 =======
 import React from 'react';
 import * as styles from './home-page.css';
-
+import { button } from '../../shared/components/button/button.css';
 const IS_LOGGED_IN = true;
 >>>>>>> d9a8656 (feat: homepage 일부 구성)
 
@@ -24,14 +24,16 @@ const HomePage = () => {
 };
 =======
     <header className={styles.header}>
-      <div>
-        <span className={styles.logo}>FINI</span>
+      <div className={styles.logoContainer}>
+        <button className={styles.logo} onClick={() => window.location.href = '/'}>
+          <span className={styles.logo}>FINI</span>
+        </button>
+        {IS_LOGGED_IN && (
+        <button className={styles.menuButton} onClick={handleMenuToggle}>
+          ☰
+        </button>
+        )}
       </div>
-      {IS_LOGGED_IN && (
-      <button className={styles.menuButton} onClick={handleMenuToggle}>
-        ☰
-      </button>
-      )}
       {IS_LOGGED_IN && isMenuOpen && (
       <nav className={styles.dropdownMenu}>
         <ul>
@@ -42,8 +44,12 @@ const HomePage = () => {
       </nav>
       )}
     </header>
-    <main style = {{ padding: '20px' }}>
-      <h1 className={styles.title}>main</h1>
+
+    <main className={styles.Main}>
+      <div className={styles.MainButtonWrapper}>
+        <button className={`${button({variant:'blue'})} ${styles.MainButton}`}>예금</button>
+        <button className={`${button({variant:'blue'})} ${styles.MainButton}`}>적금</button>
+      </div>
     </main>
     </>
 )};
