511fe11d6da26f85849dc6b1639653ee9414e514
1jiwoo27 <chloey1227@naver.com>
2025-11-15 01:16:27 +0900

    feat: 회원가입 라우터 연결

--- src/shared/router/index.tsx
+++ src/shared/router/index.tsx
@@ -1,11 +1,13 @@
-import { createBrowserRouter } from "react-router-dom";
-import { PATH } from "./path";
-import HomePage from "../../pages/home/home-page";
-import LoginPage from "../../pages/login/login-page";
-import ErrorPage from "../../pages/error/error-page";
+import { createBrowserRouter } from 'react-router-dom';
+import { PATH } from './path';
+import HomePage from '../../pages/home/home-page';
+import LoginPage from '../../pages/login/login-page';
+import SignupPage from '../../pages/signup/signup-page';
+import ErrorPage from '../../pages/error/error-page';
 
 export const router = createBrowserRouter([
   { path: PATH.HOME, element: <HomePage /> },
   { path: PATH.LOGIN, element: <LoginPage /> },
+  { path: PATH.SIGNUP, element: <SignupPage /> },
   { path: PATH.ERROR, element: <ErrorPage /> },
 ]);
--- src/shared/router/path.ts
+++ src/shared/router/path.ts
@@ -1,5 +1,6 @@
 export const PATH = {
-  HOME: "/",
-  LOGIN: "/login",
-  ERROR: "*",
+  HOME: '/',
+  LOGIN: '/login',
+  SIGNUP: '/signup',
+  ERROR: '*',
 } as const;
