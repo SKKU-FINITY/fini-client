acbf7324dce8a34eaae8eacf68fe188396f93d04
1jiwoo27 <chloey1227@naver.com>
2025-11-18 22:38:09 +0900

    feat: 헤더 컴포넌트 구현 및 로그아웃 api 연동

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,7 +1,13 @@
 import * as styles from './home-page.css';
+import Header from '../../shared/components/header/header';
 
 const HomePage = () => {
-  return <div className={styles.title}>홈 페이지</div>;
+  return (
+    <>
+      <Header />
+      <div className={styles.title}>홈 페이지</div>
+    </>
+  );
 };
 
 export default HomePage;
