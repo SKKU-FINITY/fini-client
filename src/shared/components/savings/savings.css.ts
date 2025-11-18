36f7713e5ca918102b1325246592c10274fd4fef
1jiwoo27 <chloey1227@naver.com>
2025-11-18 23:19:33 +0900

    feat: props 추가

--- src/shared/components/savings/savings.css.ts
+++ src/shared/components/savings/savings.css.ts
@@ -5,7 +5,7 @@ export const savingsContainer = style({
   display: 'flex',
   flexDirection: 'column',
   width: '65rem',
-  gap: '1.6rem',
+  gap: '1rem',
   padding: '2rem 3rem',
   border: `1px solid ${vars.color.blue300}`,
   borderRadius: '10px',
--- src/shared/components/savings/savings.tsx
+++ src/shared/components/savings/savings.tsx
@@ -1,26 +1,42 @@
 import * as styles from './savings.css';
 
-const Savings = () => {
+interface SavingsProps {
+  bankName: string;
+  productName: string;
+  rsrvTypeNm: string;
+  saveTerm: number;
+  baseRate: number;
+  maxRate: number;
+}
+
+const Savings = ({
+  bankName,
+  productName,
+  rsrvTypeNm,
+  saveTerm,
+  baseRate,
+  maxRate,
+}: SavingsProps) => {
   return (
     <div className={styles.savingsContainer}>
-      <div className={styles.bank}>수협은행</div>
-      <div className={styles.product}>Sh해양플라스틱Zero!적금</div>
+      <div className={styles.bank}>{bankName}</div>
+      <div className={styles.product}>{productName}</div>
       <div className={styles.detail}>
         <div className={styles.detailContainer}>
           <div className={styles.title}>유형</div>
-          <div className={styles.value}>자유적립식</div>
+          <div className={styles.value}>{rsrvTypeNm}</div>
         </div>
         <div className={styles.detailContainer}>
           <div className={styles.title}>기간</div>
-          <div className={styles.value}>12개월</div>
+          <div className={styles.value}>{saveTerm}개월</div>
         </div>
         <div className={styles.detailContainer}>
           <div className={styles.title}>기본 금리</div>
-          <div className={styles.value}>3.5%</div>
+          <div className={styles.value}>{baseRate}%</div>
         </div>
         <div className={styles.detailContainer}>
           <div className={styles.title}>최대 우대 금리</div>
-          <div className={styles.value}>4%</div>
+          <div className={styles.value}>{maxRate}%</div>
         </div>
       </div>
     </div>
