bc3bed193d6de6cbcd71e7e49b59346e27f36ce6
1jiwoo27 <chloey1227@naver.com>
2025-11-19 21:34:10 +0900

    feat: 예금 상세 페이지 구현

---
+++ src/pages/deposit-detail/deposit-detail-page.css.ts
@@ -0,0 +1,95 @@
+import { vars } from '../../styles/theme.css';
+import { style } from '@vanilla-extract/css';
+
+export const detailContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '70rem',
+  gap: '1rem',
+  border: `1px solid ${vars.color.pink300}`,
+  borderRadius: '10px',
+  padding: '2rem 3rem',
+  marginBottom: '2rem',
+  marginTop: '9rem',
+});
+
+export const bank = style({
+  fontSize: vars.size.md,
+  fontWeight: vars.weight.bold,
+  color: vars.color.pink300,
+});
+
+export const product = style({
+  fontSize: vars.size.xl,
+  fontWeight: vars.weight.bold,
+  color: vars.color.pink300,
+  paddingBottom: '1rem',
+});
+
+export const detailTextContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1rem',
+  paddingBottom: '1.5rem',
+});
+
+export const textContainer = style({
+  display: 'flex',
+  gap: '0.5rem',
+});
+
+export const textTitle = style({
+  fontSize: vars.size.sm,
+  fontWeight: vars.weight.medium,
+  color: vars.color.pink300,
+});
+
+export const textValue = style({
+  fontSize: vars.size.sm,
+  fontWeight: vars.weight.regular,
+  color: vars.color.gray800,
+});
+
+export const pre = style({
+  whiteSpace: 'pre-wrap',
+  fontFamily: 'inherit',
+  fontSize: vars.size.xs,
+  color: vars.color.gray800,
+  lineHeight: '2rem',
+});
+
+export const optionContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  borderTop: `1.2px solid ${vars.color.pink300}`,
+  paddingTop: '1.5rem',
+});
+
+export const optionTextContainer = style({
+  display: 'flex',
+  gap: '0.5rem',
+  paddingTop: '0.7rem',
+});
+
+export const optionText = style({
+  fontSize: vars.size.md,
+  fontWeight: vars.weight.bold,
+  color: vars.color.pink300,
+});
+
+export const optionTitle = style({
+  fontSize: vars.size.ms,
+  fontWeight: vars.weight.medium,
+  color: vars.color.pink300,
+});
+
+export const optionValue = style({
+  fontSize: vars.size.ms,
+  fontWeight: vars.weight.regular,
+  color: vars.color.gray800,
+});
+
+export const similarProductsContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+});
