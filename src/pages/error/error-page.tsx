e5bf956aa6605345e27931282471deeb522d8913
1jiwoo27 <chloey1227@naver.com>
2025-11-23 00:11:38 +0900

    feat: error page 구현

--- src/pages/error/error-page.tsx
+++ src/pages/error/error-page.tsx
@@ -1,5 +1,21 @@
+import * as styles from './error-page.css';
+import { useNavigate } from 'react-router-dom';
+import { PATH } from '../../shared/router/path';
+import Button from '../../shared/components/button/button';
+
 const ErrorPage = () => {
-  return <div>에러 페이지</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.icon}>⚠️</div>
+      <div className={styles.title}>문제가 발생했습니다.</div>
+      <div className={styles.subtitle}>주소를 다시 확인해주세요.</div>
+      <div className={styles.buttonWrapper}>
+        <Button onClick={() => navigate(PATH.HOME)}>홈으로 돌아가기</Button>
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
