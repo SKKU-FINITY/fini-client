c1dd0c7caa51d613997ca1ecb2bc881bf54b8092
1jiwoo27 <chloey1227@naver.com>
2025-11-23 00:14:07 +0900

    feat: 헤더 추가

--- src/pages/error/error-page.tsx
+++ src/pages/error/error-page.tsx
@@ -2,19 +2,23 @@ import * as styles from './error-page.css';
 import { useNavigate } from 'react-router-dom';
 import { PATH } from '../../shared/router/path';
 import Button from '../../shared/components/button/button';
+import Header from '../../shared/components/header/header';
 
 const ErrorPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div className={styles.container}>
-      <div className={styles.icon}>⚠️</div>
-      <div className={styles.title}>문제가 발생했습니다.</div>
-      <div className={styles.subtitle}>주소를 다시 확인해주세요.</div>
-      <div className={styles.buttonWrapper}>
-        <Button onClick={() => navigate(PATH.HOME)}>홈으로 돌아가기</Button>
+    <>
+      <Header />
+      <div className={styles.container}>
+        <div className={styles.icon}>⚠️</div>
+        <div className={styles.title}>문제가 발생했습니다.</div>
+        <div className={styles.subtitle}>주소를 다시 확인해주세요.</div>
+        <div className={styles.buttonWrapper}>
+          <Button onClick={() => navigate(PATH.HOME)}>홈으로 돌아가기</Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
